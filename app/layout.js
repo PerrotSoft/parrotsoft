606efcf08a4aeaaefc54e8c5567e40ab1ae01659
ParrotSoft <153956765+PerrotSoft@users.noreply.github.com>
2026-04-21 19:11:11 +0300

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css';
 import ClientInterface from './ClientInterface';
 import { createClient } from '@libsql/client';
-
+export const dynamic = 'force-dynamic';
 const client = createClient({
   url: "libsql://parrotsoft-vercel-icfg-i713yoki8d1eytlkyrwlsfzr.aws-us-east-1.turso.io",
   authToken: "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE3NzEzNjM2NjIsImlkIjoiN2YyYTY2MDgtYWZjOC00MTQ1LWFlNmYtZDljMDhkZGRhZWE3IiwicmlkIjoiZDU5ZjM3ZTYtZGE5YS00YTA2LTk4OWYtMTBhYTRjNWFmOTViIn0.V6NDZo1wMJNNs5ipc40YkuTCXqG4DwijLBkqtDbr-6_uJa1xCJvHPOvE3jeK2UOfTBtc-cD8SZ0s3tqALRuABA",
